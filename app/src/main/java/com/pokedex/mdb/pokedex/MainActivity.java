7036158e2dd901250b418182dec6695d9581f93d
Katharine Jiang <katharine.jiang9@gmail.com>
2016-09-29 02:00:00 -0700

    wrote search, don't know if it works

--- app/src/main/java/com/pokedex/mdb/pokedex/MainActivity.java
+++ app/src/main/java/com/pokedex/mdb/pokedex/MainActivity.java
@@ -2,12 +2,22 @@ package com.pokedex.mdb.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ArrayList<com.demo.mdb.pokedex.Pokedex.Pokemon> pokemon = com.demo.mdb.pokedex.Pokedex.getPokemon();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
