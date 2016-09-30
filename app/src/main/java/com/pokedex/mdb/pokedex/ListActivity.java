268e88e6279a2a6e688128e42bf1434dd5dd32cd
Katharine Jiang <katharine.jiang9@gmail.com>
2016-09-29 19:59:01 -0700

    have all the files to work with

---
+++ app/src/main/java/com/pokedex/mdb/pokedex/ListActivity.java
@@ -0,0 +1,83 @@
+package com.pokedex.mdb.pokedex;
+
+/**
+ * Created by Aayush on 9/28/2016.
+ */
+
+        import android.app.SearchManager;
+        import android.content.Context;
+        import android.support.v4.view.MenuItemCompat;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.widget.GridLayoutManager;
+        import android.support.v7.widget.LinearLayoutManager;
+        import android.support.v7.widget.RecyclerView;
+        import android.support.v7.widget.SearchView;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
+        import android.widget.Toast;
+
+        import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+
+    private RecyclerView recycleboy;
+    private boolean isLinear = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        recycleboy = (RecyclerView)findViewById(R.id.recycleboy);
+        if(isLinear){recycleboy.setLayoutManager(new LinearLayoutManager(this));}
+        else {recycleboy.setLayoutManager(new GridLayoutManager(this, 2));}
+
+        Pokedex pokedex = new Pokedex();
+        ArrayList<Pokedex.Pokemon> pokemons = pokedex.getPokemon();
+        Pokedapter pokedapter = new Pokedapter(getApplicationContext(), pokemons);
+        recycleboy.setAdapter(pokedapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_list, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
+
+        return true;
+    }
+
+    public void switchLayout(MenuItem item){
+        if(!isLinear){recycleboy.setLayoutManager(new LinearLayoutManager(this));}
+        else {recycleboy.setLayoutManager(new GridLayoutManager(this, 2));}
+        isLinear = !isLinear;
+    }
+/*
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_list, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this)
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        // User pressed the search button
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        // User changed the text
+        return false;
+    }*/
+}
---
+++ app/src/main/java/com/pokedex/mdb/pokedex/PokemonActivity.java
@@ -0,0 +1,63 @@
+package com.pokedex.mdb.pokedex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+public class PokemonActivity extends AppCompatActivity {
+
+    private String name;
+    private String url;
+    private TextView number;
+    private TextView attack;
+    private TextView defense;
+    private TextView hp;
+    private TextView species;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        number = (TextView)findViewById(R.id.number);
+        attack = (TextView)findViewById(R.id.attack);
+        defense = (TextView)findViewById(R.id.defense);
+        hp = (TextView)findViewById(R.id.hp);
+        species = (TextView)findViewById(R.id.species);
+        img = (ImageView)findViewById(R.id.imageView);
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+
+        setTitle(name);
+        url = (String)b.get("url");
+        Glide.with(getApplicationContext()).load(url).thumbnail(0.5f).crossFade().diskCacheStrategy(DiskCacheStrategy.ALL).into(img);
+        number.setText("Number: #" + b.get("number"));
+        attack.setText("Attack: " + b.get("attack"));
+        defense.setText("Defense: " + b.get("defense"));
+        hp.setText("HP: " + b.get("hp"));
+        species.setText("Species: " + b.get("species"));
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with online search", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+}
